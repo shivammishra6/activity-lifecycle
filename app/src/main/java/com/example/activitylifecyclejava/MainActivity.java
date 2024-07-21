49efbe5d18e0175b5f09397925c57a1708e6b0a1
shivammishra6 <shivammmishra6203@gmail.com>
2024-07-21 18:38:01 +0530

    added second activity and implemented lifecycle methods

--- app/src/main/java/com/example/activitylifecyclejava/MainActivity.java
+++ app/src/main/java/com/example/activitylifecyclejava/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.activitylifecyclejava;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.d("shivam", "onCreate: ");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("shivam", "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("shivam", "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("shivam", "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("shivam", "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("shivam", "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("shivam", "onDestroy: ");
+    }
+
+    public void send(View view) {
+        Intent i =new Intent(this, MainActivity2.class);
+        startActivity(i);
     }
 }
